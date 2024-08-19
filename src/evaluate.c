50cc3c725302af04adec0734f44539501afa9188
dc5v <dev@dc5v.com>
2024-08-20 06:11:52 +0900

    evaluate.c format

--- src/evaluate.c
+++ src/evaluate.c
@@ -2,38 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define EVA_CV_P2                 0b00001111
-#define EVA_DEVIATION_MAXIMUM_P2  0b00010001
-#define EVA_DEVIATION_STANDARD_P2 0b00010101
-#define EVA_ENTROPY_BINARY_P1     0b00000001
-#define EVA_EXPECTED_P1           0b00000010
-#define EVA_IQR_P1                0b00000101
-#define EVA_KURTOSIS_P3           0b00011001
-#define EVA_MAD_P2                0b00010000
-#define EVA_MAE_P1                0b00000110
-#define EVA_MEAD_P1               0b00000111
-#define EVA_MEAN_GEOMETRIC_P1     0b00000011
-#define EVA_MEAN_HARMONIC_P1      0b00000100
-#define EVA_MEAN_P1               0b00001000
-#define EVA_MEAN_TRIMMED_P2       0b00010110
-#define EVA_MEDIAN_P1             0b00001001
-#define EVA_MODE_P1               0b00001010
-#define EVA_MSE_P2                0b00010010
-#define EVA_PERCENTILE_P2         0b00010100
-#define EVA_QUARTILES_P4R3        0b00011010
-#define EVA_RANGE_P1              0b00001100
-#define EVA_RANGE_PERCENT_P1      0b00001011
-#define EVA_RMS_P1                0b00001101
-#define EVA_RMSLE_P1              0b00001110
-#define EVA_SKEWNESS_M_P2         0b00010011
-#define EVA_SKEWNESS_Z_P2         0b00011000
-#define EVA_VARIANCE_P2           0b00010111
+#define EVA_CV_P2                 0b00001111 // 변동계수 (Coefficient of Variation, CV)
+#define EVA_DEVIATION_MAXIMUM_P2  0b00010001 // 최대차이 (Maximum Deviation)
+#define EVA_DEVIATION_STANDARD_P2 0b00010101 // 표준편차 (Standard Deviation)
+#define EVA_ENTROPY_BINARY_P1     0b00000001 // 이진 엔트로피 (Binary Entropy)
+#define EVA_EXPECTED_P1           0b00000010 // 기대값 (Expected Value)
+#define EVA_IQR_P1                0b00000101 // 사분위범위 (Interquartile Range, IQR)
+#define EVA_KURTOSIS_P3           0b00011001 // 첨도 (Kurtosis)
+#define EVA_MAD_P2                0b00010000 // 평균 절대 편차 (Mean Absolute Deviation, MAD)
+#define EVA_MAE_P1                0b00000110 // 평균 절대 편차 (Mean Absolute Error, MAE)
+#define EVA_MEAD_P1               0b00000111 // 절대 편차 중앙값 (Median Absolute Deviation, MeAD)
+#define EVA_MEAN_GEOMETRIC_P1     0b00000011 // 기하평균 (Geometric Mean)
+#define EVA_MEAN_HARMONIC_P1      0b00000100 // 조화평균 (Harmonic Mean)
+#define EVA_MEAN_P1               0b00001000 // 평균값 (Mean)
+#define EVA_MEAN_TRIMMED_P2       0b00010110 // 절사평균 (Trimmed Mean)
+#define EVA_MEDIAN_P1             0b00001001 // 중위값 (Median)
+#define EVA_MODE_P1               0b00001010 // 최빈값 (Mode)
+#define EVA_MSE_P2                0b00010010 // 평균 제곱 편차 (Mean Squared Error, MSE)
+#define EVA_PERCENTILE_P2         0b00010100 // 백분위수 (Percentiles)
+#define EVA_QUARTILES_P4R3        0b00011010 // 사분위수 (Quartiles)
+#define EVA_RANGE_P1              0b00001100 // 범위 (Range)
+#define EVA_RANGE_PERCENT_P1      0b00001011 // 퍼센트 범위 (PercentRange)
+#define EVA_RMS_P1                0b00001101 // 제곱평균 (Root Mean Square, RMS)
+#define EVA_RMSLE_P1              0b00001110 // 제곱평균 로그 (RMSLE)
+#define EVA_SKEWNESS_M_P2         0b00010011 // 중위값 기준 왜도 (Median Skewness)
+#define EVA_SKEWNESS_Z_P2         0b00011000 // 왜도(Skewness)
+#define EVA_VARIANCE_P2           0b00010111 // 분산 (Variance)
 
 static inline void eva_swap ( double *a, double *b )
 {
   double temp = *a;
-  *a          = *b;
-  *b          = temp;
+
+  *a = *b;
+  *b = temp;
 }
 
 static void eva_sort ( double *data, int left, int right )
@@ -94,8 +95,8 @@ double eva_p1 ( int operation, size_t len, const double *data )
       }
 
       result = valid_count > 0 ? pow ( product, 1.0 / valid_count ) : 0.0;
-      break;
     }
+    break;
 
     case EVA_MEAN_HARMONIC_P1:
     {
@@ -112,8 +113,8 @@ double eva_p1 ( int operation, size_t len, const double *data )
       }
 
       result = valid_count > 0 ? valid_count / sum_reciprocal : 0.0;
-      break;
     }
+    break;
 
     case EVA_MEDIAN_P1:
     {
@@ -132,13 +133,14 @@ double eva_p1 ( int operation, size_t len, const double *data )
       }
 
       free ( sorted_data );
-      break;
     }
+    break;
 
     case EVA_MODE_P1:
     {
       double mode       = data[0];
       int    mode_count = 1, max_count = 1;
+
       for ( size_t i = 1; i < len; i++ )
       {
         if ( data[i] == data[i - 1] )
@@ -152,108 +154,125 @@ double eva_p1 ( int operation, size_t len, const double *data )
             max_count = mode_count;
             mode      = data[i - 1];
           }
+
           mode_count = 1;
         }
       }
       result = mode;
-      break;
     }
+    break;
 
     case EVA_RANGE_P1:
     {
       double min_val = data[0];
       double max_val = data[0];
+
       for ( size_t i = 1; i < len; i++ )
       {
         if ( data[i] < min_val )
         {
           min_val = data[i];
         }
+
         if ( data[i] > max_val )
         {
           max_val = data[i];
         }
       }
+
       result = max_val - min_val;
-      break;
     }
+    break;
 
     case EVA_IQR_P1:
     {
       double q1 = eva_p2 ( EVA_PERCENTILE_P2, len, data, 25.0 );
       double q3 = eva_p2 ( EVA_PERCENTILE_P2, len, data, 75.0 );
-      result    = q3 - q1;
-      break;
+
+      result = q3 - q1;
     }
+    break;
 
     case EVA_RMS_P1:
     {
       double sum_of_squares = 0.0;
+
       for ( size_t i = 0; i < len; i++ )
       {
         sum_of_squares += data[i] * data[i];
       }
+
       result = sqrt ( sum_of_squares / len );
-      break;
     }
+    break;
 
     case EVA_RMSLE_P1:
     {
       double sum_of_squares = 0.0;
+
       for ( size_t i = 0; i < len; i++ )
       {
         sum_of_squares += pow ( log ( data[i] + 1 ), 2 );
       }
+
       result = sqrt ( sum_of_squares / len );
-      break;
     }
+    break;
 
     case EVA_MAE_P1:
     {
       double sum_of_absolute_errors = 0.0;
+
       for ( size_t i = 0; i < len; i++ )
       {
         sum_of_absolute_errors += fabs ( data[i] );
       }
+
       result = sum_of_absolute_errors / len;
-      break;
     }
+    break;
 
     case EVA_MEAD_P1:
     {
       double  median             = eva_p1 ( EVA_MEDIAN_P1, len, data );
       double *absolute_deviation = malloc ( len * sizeof ( double ) );
+
       for ( size_t i = 0; i < len; i++ )
       {
         absolute_deviation[i] = fabs ( data[i] - median );
       }
+
       result = eva_p1 ( EVA_MEDIAN_P1, len, absolute_deviation );
       free ( absolute_deviation );
-      break;
     }
+    break;
 
     case EVA_RANGE_PERCENT_P1:
     {
       double min_val = data[0];
       double max_val = data[0];
+
       for ( size_t i = 1; i < len; i++ )
       {
         if ( data[i] < min_val )
         {
           min_val = data[i];
         }
+
         if ( data[i] > max_val )
         {
           max_val = data[i];
         }
       }
+
       result = ( ( max_val - min_val ) / max_val ) * 100.0;
-      break;
     }
+    break;
 
     case EVA_ENTROPY_BINARY_P1:
     {
       double entropy = 0.0;
+
       for ( size_t i = 0; i < len; i++ )
       {
         if ( data[i] > 0 )
@@ -261,12 +280,10 @@ double eva_p1 ( int operation, size_t len, const double *data )
           entropy -= data[i] * log2 ( data[i] );
         }
       }
-      result = entropy / len;
-      break;
-    }
 
-    default:
-      break;
+      result = entropy / len;
+    }
+    break;
   }
 
   return result;
@@ -285,13 +302,16 @@ double eva_p2 ( int operation, size_t len, const double *data, const double p1 )
       {
         result += ( data[i] - p1 ) * ( data[i] - p1 );
       }
+
       result /= len;
-      break;
     }
+    break;
 
     case EVA_DEVIATION_STANDARD_P2:
+    {
       result = sqrt ( eva_p2 ( EVA_VARIANCE_P2, len, data, p1 ) );
-      break;
+    }
+    break;
 
     case EVA_MAD_P2:
     {
@@ -302,16 +322,16 @@ double eva_p2 ( int operation, size_t len, const double *data, const double p1 )
       }
       result = eva_p1 ( EVA_MEDIAN_P1, len, absolute_deviation );
       free ( absolute_deviation );
-      break;
     }
+    break;
 
     case EVA_PERCENTILE_P2:
     {
       double k = ( p1 / 100.0 ) * ( len - 1 );
       size_t f = ( size_t )floor ( k ), c = ( size_t )ceil ( k );
       result = data[f] + ( k - f ) * ( data[c] - data[f] );
-      break;
     }
+    break;
 
     case EVA_DEVIATION_MAXIMUM_P2:
     {
@@ -319,38 +339,41 @@ double eva_p2 ( int operation, size_t len, const double *data, const double p1 )
       {
         double deviation = fabs ( data[i] - p1 );
         if ( deviation > result )
+        {
           result = deviation;
+        }
       }
-      break;
     }
+    break;
 
     case EVA_MEAN_TRIMMED_P2:
     {
       size_t trim_amount = ( size_t )( len * p1 );
 
       result = eva_p1 ( EVA_MEAN_P1, len - 2 * trim_amount, data + trim_amount );
-      break;
     }
+    break;
 
     case EVA_SKEWNESS_M_P2:
     case EVA_SKEWNESS_Z_P2:
     {
       double sum_cubed_deviation = 0.0;
       double stddev              = eva_p2 ( EVA_DEVIATION_STANDARD_P2, len, data, p1 );
+
       for ( size_t i = 0; i < len; i++ )
       {
         sum_cubed_deviation += pow ( data[i] - p1, 3 );
       }
+
       result = ( sum_cubed_deviation / len ) / pow ( stddev, 3 );
-      break;
     }
+    break;
 
     case EVA_CV_P2:
+    {
       result = p1 / eva_p2 ( EVA_DEVIATION_STANDARD_P2, len, data, p1 );
-      break;
-
-    default:
-      break;
+    }
+    break;
   }
 
   return result;
